58d9cfbaf9e5f532463790a802660c3febae779a
GJ <deathsos@gmail.com>
2025-06-23 20:30:02 +0100

    Tidy up

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,19 @@ const WEBPAGE_ROOT: &'static str = "webpages/";
 const SERVICE_ROOT: &'static str = "service_files/";
 
 const WEBPAGE_INDEX: &str = concat_const::concat!(WEBPAGE_ROOT, "hello.html");
-const WEBPAGE_E404: &str = concat_const::concat!(WEBPAGE_ROOT, "404.html");
+const WEBPAGE_E404:  &str = concat_const::concat!(WEBPAGE_ROOT, "404.html");
 const WEBPAGE_FILES: &str = concat_const::concat!(WEBPAGE_ROOT, "files.html");
 
 fn main() {
+    let id_handler = Box::new(|request: web_server::Request, _response| {
+        let foo = SERVICE_ROOT.to_owned() + request.params.values().into_iter().next().unwrap();
+        Path::new(foo.as_str()).into()
+    });
     generate_files_webpage();
     web_server::new()
         .get("/", Box::new(|_, _| Path::new(WEBPAGE_INDEX).into()))
         .get("/files", Box::new(|_, _| Path::new(WEBPAGE_FILES).into()))
-        .get(
-            "/file/:id",
-            Box::new(|request: web_server::Request, _response| {
-                let foo = SERVICE_ROOT.to_owned() + request.params.values().into_iter().next().unwrap();
-                Path::new(foo.as_str()).into()
-            }),
-        )
+        .get("/file/:id", id_handler)
         .not_found(Box::new(|_,_| Path::new(WEBPAGE_E404).into()))
         .launch(PORT);
 }
@@ -37,7 +35,7 @@ fn generate_files_webpage() {
         .open(WEBPAGE_FILES)
         .unwrap();
 
-    let filenames = fs::read_dir("service_files").unwrap();
+    let filenames = fs::read_dir(SERVICE_ROOT).unwrap();
     filenames.for_each(|file| {
         let filename = file.unwrap().file_name();
         writeln!(files_webpage, "{:?}", filename).unwrap();
